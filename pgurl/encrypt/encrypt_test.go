7d9c528046b269ac44eaffd1bda8cf569e9c013d
jneo8 <james851212@gmail.com>
2018-06-05 00:51:14 +0800

    encrypt_test.go

---
+++ pgurl/encrypt/encrypt_test.go
@@ -0,0 +1,13 @@
+package encrypt
+
+import (
+    "testing"
+)
+
+func TestAES(t *testing.T) {
+        // write some test
+        key := []byte("LKHlhb899Y09olUi")
+        encryptMsg, _ := Encrypt(key, "PGURL is GOOD !!!!!")
+        msg, _ := Decrypt(key, encryptMsg)
+        t.Logf("%s -> %s", msg, encryptMsg) // Hello World
+}
