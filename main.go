9eea6a1d5bbcfce7e0225945c7143c8706fe450d
jneo8 <james851212@gmail.com>
2018-06-05 01:13:36 +0800

    encrypt in api

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"./pgurl/db"
+	"./pgurl/encrypt"
 	"./pgurl/gen"
-        "net/http"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+const key = "LKHlhb899Y09olUi"
 
 func main() {
 	r := gin.Default()
+	aes_key := []byte(key)
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -16,20 +20,25 @@ func main() {
 	})
 	r.POST("/short", func(c *gin.Context) {
 		url := c.PostForm("url")
-		hash := gen.GenRandomStr(6)
-		status := db.Create(hash, url)
+		// value, store in db.
+		hash_url, _ := encrypt.Encrypt(aes_key, url)
+
+		// key, return to user.
+		url_key := gen.GenRandomStr(6)
+		status := db.Create(url_key, hash_url)
 
 		c.JSON(200, gin.H{
 			"status":   status,
-			"shorturl": hash,
+			"shorturl": url_key,
 		})
 	})
-        r.GET("/q/:hash", func(c *gin.Context) {
-                hash := c.Param("hash")
-                url, status := db.Read(hash)
-                if (status != "err") {
-		    c.Redirect(http.StatusMovedPermanently, url)
-                }
+	r.GET("/q/:url_key", func(c *gin.Context) {
+		url_key := c.Param("url_key")
+		hash_url, status := db.Read(url_key)
+		if status != "err" {
+			url, _ := encrypt.Decrypt(aes_key, hash_url)
+			c.Redirect(http.StatusMovedPermanently, url)
+		}
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
