dbbe80ef53cc6f3d61057137ef666c73a1e96956
Herber <herber523@dailyview.tw>
2018-06-06 21:59:34 +0800

    add test redis

--- pgurl/db/setting.go
+++ pgurl/db/setting.go
@@ -6,7 +6,7 @@ var redis_client *redis.Client = RedisCon()
 
 func  RedisCon () *redis.Client{
 	client := redis.NewClient(&redis.Options{
-		Addr:     "192.168.1.31:6379",
+		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
